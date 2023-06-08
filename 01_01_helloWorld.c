dab85837448bcc70b9b26af6b246b76e8393bccf
watanabeyu1218 <k23145kk@aitech.ac.jp>
2023-06-08 18:15:07 +0900

    c言語

---
+++ 01_01_helloWorld.c
@@ -0,0 +1,13 @@
+//
+//  02_01_helloWorld.c
+//  prg1
+//
+//  Created by k23145kk on 2023/04/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]){
+    printf("Watanabe Yuji\n");
+    return 0;
+}
